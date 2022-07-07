0eed441c948512b164256f7aac3303ca3926391c
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-07-07 10:29:34 -0300

    creamos la serie, se agrega al localStorage

--- js/admin.js
+++ js/admin.js
@@ -8,8 +8,10 @@ let descripcion = document.getElementById("descripcion");
 let imagen = document.getElementById("imagen");
 let genero = document.getElementById("genero");
 let formulario = document.getElementById("formSerie");
+const modalAdminSerie = new bootstrap.Modal(document.getElementById("modalSerie"));
 
-let listaSeries = [];
+// Si hay algo en localStorage Traer datos, Si no hay nada listaSeries tiene que ser un arreglo vacio "[]"
+let listaSeries = JSON.parse(localStorage.getItem("listaSeriesKey")) || [];
 
 // Agregar validaciones
 titulo.addEventListener("blur",()=>{validacionNombre(titulo)});
@@ -23,11 +25,20 @@ function crearSerie(e){
     // Volver a validar todos los campos
     let nuevaSerie = new Serie(codigo.value, titulo.value, descripcion.value, imagen.value, genero.value);
     // Agregamos la serie al final del arreglo
-    listaSeries.push(nuevaSerie)
+    listaSeries.push(nuevaSerie);
     // limpiamos formulario
-    limpiarFormulario()
+    limpiarFormulario();
+    // Guardar la lista de series en localStorage
+    guardarListaSeries();
+    // Cerrar modal que administra series
+
 }
 
 function limpiarFormulario(){
     formulario.reset();
+    // Si usamos la clases isvalid o isInvalid de bootstrap hay que resetearlas
+}
+
+function guardarListaSeries(){
+    localStorage.setItem("listaSeriesKey", JSON.stringify(listaSeries));
 }
