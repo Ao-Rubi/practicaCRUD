bcd23f22ee44600bfee8654b2d874237de0ba148
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-07-14 11:00:02 -0300

    Se muestran las series del localStorage en la pagina principal

---
+++ js/app.js
@@ -0,0 +1,22 @@
+//  Verificar si hay datos en el localStorage
+let listaSerie = JSON.parse(localStorage.getItem("listaSeriesKey")) || [];
+
+// Si hay datos dibujar las cards
+listaSerie.forEach((serie)=>{
+    crearColumna(serie);
+})
+
+function crearColumna(serie) {
+    let grillaSeries = document.getElementById("grillaSeries");
+    grillaSeries.innerHTML += `
+        <article class="col-12 col-md-4 col-lg-3 mb-3">
+            <div class="card">
+                <img src="${serie.imagen}" class="card-img-top" alt="${serie.titulo}">
+                <div class="card-body">
+                    <h5 class="card-title">${serie.titulo}</h5>
+                    <a href="#" class="btn btn-primary">Ver Detalle</a>
+                </div>
+            </div>
+        </article>
+    `
+}
