c1599c4d7111896d980d6528ddce0f37000cdf0f
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-07-13 21:22:36 -0300

    validacion confirmada, cambio de titulo en modal html.

--- js/admin.js
+++ js/admin.js
@@ -37,12 +37,21 @@ function guardarSerie(e) {
         //quiero modificar una serie existente
     }else{
         // aqui quiero crear una nueva serie
-        crearSerie()
+        if (validacionNombre(titulo) && cantidadCaracteres(10,400,descripcion) && validacionURL(imagen)) {
+            crearSerie() 
+        }else{
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Dejaste un campo sin llenar',
+            })
+        }
+        
     }
 }
 
 function crearSerie(e){
-    e.preventDefault()
+    // e.preventDefault()
     // Volver a validar todos los campos
     let nuevaSerie = new Serie(codigo.value, titulo.value, descripcion.value, imagen.value, genero.value);
     // Agregamos la serie al final del arreglo
